f12364f7c85f8d8605ec82d52c6f01fac9ecf77d
rosyfi <103525780+rosyfi@users.noreply.github.com>
2026-05-05 17:19:12 +0200

    fix: remove duplicate Image import in About component

--- src/app/content/About.tsx
+++ src/app/content/About.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Image from "next/image";
 import styles from "./styles/About.module.css";
 
 const About = () => {
